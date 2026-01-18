cabef1aaae7507d1795a89b8876d07f7d26fb5d6
Ye Min Htet <2403803F@student.tp.edu.sg>
2026-01-18 22:16:58 +0800

    Complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,62 @@
 import { useEffect, useState } from "react"
 import { fetchCountries } from "./api"
 import type { Country } from "./type"
+import "./App.css"
+import { Input } from "./components/ui/input"
+import { Button } from "./components/ui/button"
 
 function App() {
   const [countries,setCountries] = useState<Country[]>([])
-  useEffect(() => {
-    const loadCountries = async () => {
-      const data = await fetchCountries("oceania")
+  const [searchQuery, setSearchQuery] = useState("asia")
+  const [loading, setLoading] = useState(false)
+  const loadCountries = async (query: string) => {
+    setLoading(true)
+    try{
+      const data = await fetchCountries(query)
       if (typeof data !== "string") {
         setCountries(data)
         }
+    }finally{
+      setLoading(false)
     }
-    loadCountries()},[]);
+      
+    }
+  useEffect(() => {
+    loadCountries(searchQuery)
+  }, [])
+
+  const handleSearch = () => {
+    loadCountries(searchQuery)
+  }
+    
 
   return (
     <>
-       <ul>
-        {countries.map(c => (
-          <li> <img src={c.flags['png']}/> {c.name['common']} | {c.population} | {c.capital} | {c.region} | {c.subregion} </li>
-        ))}
-       </ul>
+      
+       <div className="w-full h-full flex justify-center items-center flex-col bg-[#081621] gap-5">
+        <div className="w-full h-25 flex flex-col items-center justify-center gap-5">
+           <Input className="w-75 mt-5 text-white text-2xl" type="text" value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        placeholder="Enter region (e.g., asia, europe)"/>
+           <Button onClick={handleSearch} variant="outline">{loading ? "Loading..." : "Search"}</Button>
+        </div>
+       
+          <div className="grid grid-cols-3 gap-5">
+            {loading && <div className="card">Loading countries...</div>}
+            {!loading && countries.map(c => (
+              <div className="card">
+                  <div className="card__img">
+                    <img className="w-[230px] h-[150px]" src={c.flags.png} alt="" />
+                  </div>
+                  <div className="card__subtitle">{c.region}</div>
+                  <div className="card__subtitle"> {c.subregion}</div>
+                  <div className="card__title text-green-700">{c.name.common}</div>
+                  <div className="card__subtitle">Capital: {c.capital}</div>
+                  <div className="card__subtitle">Population: {c.population}</div>
+              </div>
+            ))}
+          </div>
+       </div>
     </>
   )
 }
